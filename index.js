eb510272e4d161f46f6b1d934e93773817349e1b
baazarova <iskandarovaogiloy@gmail.com>
2022-10-24 17:17:42 +0500

    6-dars-vazifasi

---
+++ index.js
@@ -0,0 +1,58 @@
+//CALCULATOR:
+
+// let num1 = Number(prompt('1-qiymatni kiriting'));
+// let action = prompt('arifmetik amal kriting');
+// let num2 = Number(prompt('2-qiymatni kiriting'));
+
+
+
+// function getNumber (son1, action, son2){
+ 
+//  return action(son1,son2);
+// }
+// const arifmetika = function(a, b){
+//     if(action == '+'){
+//         return a + b;
+//     }
+//     else if(action == '-'){
+//         return a - b;
+//     }
+//     else if(action == '*'){
+//         return a * b;
+//     }
+//     else if(action == '/'){
+//         return a / b;
+//     }
+    
+    
+// }
+
+
+// const result = getNumber(num1, arifmetika, num2);
+
+// alert(result);
+
+
+
+//OBJECT
+
+// const user = {
+//    firstName: "O'g'iloy",
+//    lastName: "Iskandarova",
+//    age: 21,
+//    location: "Uzbekistan",
+//    massage: () => {
+//     console.log(user.lastName);
+//    } 
+// }
+
+// user.lastName = "Bozorova";
+
+// delete user.age;
+
+// user.age = 22;
+
+// user.fullName = function(){
+//     console.log(user.firstName + '' + user.lastName);
+// }
+// console.log(user.fullName);
